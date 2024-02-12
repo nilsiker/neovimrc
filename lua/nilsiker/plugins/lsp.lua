96177316d0a32b47ad51f7ca0ac3eecfe58bfa41
nilsiker <nilsiker94@gmail.com>
2024-02-12 23:22:03 +0100

    add autoclosing and emmet_ls

--- lua/nilsiker/plugins/lsp.lua
+++ lua/nilsiker/plugins/lsp.lua
@@ -34,6 +34,7 @@ return {
                 "lua_ls",
                 "tsserver",
                 "jsonls",
+                "emmet_ls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
@@ -68,6 +69,23 @@ return {
                         }
                     }
                 end,
+
+                ["emmet_ls"] = function()
+                    local lspconfig = require('lspconfig')
+                    lspconfig.emmet_ls.setup({
+                        -- on_attach = on_attach,
+                        capabilities = capabilities,
+                        filetypes = { "rust", "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+                        init_options = {
+                            html = {
+                                options = {
+                                    -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+                                    ["bem.enabled"] = true,
+                                },
+                            },
+                        }
+                    })
+                end
             }
         })
 
