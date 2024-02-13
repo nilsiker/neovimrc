b794aacd802b4cadf96c5655efe81158c0be626d
nilsiker <nilsiker94@gmail.com>
2024-02-13 18:26:12 +0100

    remap, add input for checkout

--- lua/nilsiker/plugins/vim-fugitive.lua
+++ lua/nilsiker/plugins/vim-fugitive.lua
@@ -2,10 +2,12 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "<leader>ga", function() vim.cmd("Git add *") end)
+        vim.keymap.set("n", "<leader>gA", function() vim.cmd("Git add *") end)
         vim.keymap.set("n", "<leader>gd", function() vim.cmd("Git diff") end)
-        vim.keymap.set("n", "<leader>gc", function() vim.cmd("Git commit") end)
-        vim.keymap.set("n", "<leader>gp", function() vim.cmd("Git push") end)
+        vim.keymap.set("n", "<leader>gC", function() vim.cmd("Git commit") end)
+        vim.keymap.set("n", "<leader>gP", function() vim.cmd("Git push") end)
+        vim.keymap.set("n", "<leader>gb", function() vim.cmd("Git branch") end)
+        vim.keymap.set("n", "<leader>gc", function() vim.cmd("Git checkout " .. vim.fn.input("Checkout: ")) end)
     end
 
 }
